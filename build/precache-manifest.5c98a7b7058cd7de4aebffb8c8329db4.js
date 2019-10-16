d97c5fdf3787e563821ade910423a9652928284d
apoyraz <adpoyraz@hotmail.com>
2019-10-15 22:45:48 -0400

    first commit

---
+++ build/precache-manifest.5c98a7b7058cd7de4aebffb8c8329db4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d30626e9c26531f118b6c8b2f65eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5676f0f42688c9268304",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "138ce3a88c1e935ed16a",
+    "url": "/static/js/2.87a2e41a.chunk.js"
+  },
+  {
+    "revision": "5676f0f42688c9268304",
+    "url": "/static/js/main.d4503fc5.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
